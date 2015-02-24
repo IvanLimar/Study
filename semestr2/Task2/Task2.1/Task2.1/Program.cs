fc4bb668879bfbe211cd9c5cffccc124508a91fc
IvanLimar <limvan@mail.ru>
2015-02-24 20:05:44 +0300

    Class stack

---
+++ semestr2/Task2/Task2.1/Task2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            for (int i = 0; i < 10; ++i)
+            {
+                stack.Push(i);
+            }
+            while (!stack.IsEmpty())
+            {
+                stack.Pop();
+            }
+        }
+    }
+}
---
+++ semestr2/Task2/Task2.1/Task2.1/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task2._1
+{
+    class Stack
+    {
+        private class StackElement
+        {
+            public int value;
+            public StackElement next;
+        }
+
+        private StackElement head;
+
+        public Stack()
+        {
+            this.head = null;
+        }
+
+        public Stack(int element)
+        {
+            StackElement newElement = new StackElement();
+            this.head = newElement;
+            newElement.next = null;
+            newElement.value = element;
+        }
+
+        public void Push(int element)
+        {
+            StackElement newElement = new StackElement();
+            newElement.value = element;
+            newElement.next = this.head;
+            this.head = newElement;
+        }
+
+        public int Pop()
+        {
+            if (this.IsEmpty())
+            {
+                Console.WriteLine("Stack is empty!");
+                return -9999;
+            }
+            int result = this.head.value;
+            this.head = this.head.next;
+            return result;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.head == null;
+        }
+    }
+}
