afa4ac2e63cb3ba7766fbd10fac1987be965de03
IvanLimar <limvan@mail.ru>
2015-03-05 23:18:35 +0300

    Доьавлены проперти, исправлены замечания + юнит тесты.

---
+++ semestr2/Task2/Task2.1/Stack.Tests/Stack.Test.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Stack.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Task2._1;
+
+    [TestClass]
+    public class StackTest
+    {
+        private Stack stack;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+           stack = new Stack();
+        }
+
+        [TestMethod]
+        public void PopFromEmptyTest()
+        {
+            Assert.AreEqual(-9999, stack.Pop());
+        }
+
+        [TestMethod]
+        public void PushPopTest()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.IsFalse(stack.IsEmpty());
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        [TestMethod]
+        public void IsEmptyTest()
+        {
+            Assert.IsTrue(stack.IsEmpty());
+        }
+    }
+}
