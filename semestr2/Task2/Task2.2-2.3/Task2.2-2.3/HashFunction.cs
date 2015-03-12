c9aedcda156ee760ee08e7e8394eaf05210bbb1a
Ivan Limar <limvan@mail.ru>
2015-03-12 23:57:11 +0300

    Модифицированная хеш-таблица. Хеш-функция - отдельный класс.

---
+++ semestr2/Task2/Task2.2-2.3/Task2.2-2.3/HashFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2._2_2._3
+{
+    public static class HashFunction
+    {
+        /// <summary>
+        /// Хеш-функция
+        /// </summary>
+        public static int Value(HashTable hashTable, string line)
+        {
+            int length = hashTable.Length;
+            int result = 0;
+            for (int i = 0; i < line.Length; ++i)
+            {
+                int number = Convert.ToInt32(line[i]);
+                double temp = Math.Pow(number, i) % length;
+                result = result + Convert.ToInt32(temp);
+
+            }
+            return result % length;
+        }
+    }
+}
