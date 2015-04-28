d031fc856f7e2e18e85b51b03647d9270eed5e83
Ivan Limar <limvan@mail.ru>
2015-04-28 07:31:13 +0300

    Классы NodeOperand и NodeOperation раскинуты по разным файлам

--- semestr2/Task4/Task4.1/Task4.1/Tree.cs
+++ semestr2/Task4/Task4.1/Task4.1/Tree.cs
@@ -5,7 +5,7 @@ namespace Task4_1
     /// <summary>
     /// Дерево разбора арифметического выпажения. Может печататься и считаться
     /// </summary>
-    public class Tree
+    public partial class Tree
     {
         private Node root;
 
@@ -17,107 +17,6 @@ namespace Task4_1
             public abstract string Print();
 
             public abstract int Calculate();
-        }
-
-        private class NodeOperand : Node
-        {
-            private int operand;
-
-            public NodeOperand(string[] values, ref int currentIndex)
-            {
-                operand = Int32.Parse(values[currentIndex++]);
-            }
-
-            public override string Print()
-            {
-                return operand.ToString() + " ";
-            }
-
-            public override int Calculate()
-            {
-                return operand;
-            }
-        }
-
-        private class NodeOperation : Node
-        {
-            private string operation;
-            private Node leftSon;
-            private Node rightSon;
-
-            public NodeOperation(string[] values, ref int currentIndex)
-            {
-                operation = values[currentIndex++];
-                for (int i = 0; i < 2; ++i)
-                {
-                    Node temp;
-                    while (Symbol.IsBracket(values[currentIndex]))
-                    {
-                        ++currentIndex;
-                    }
-                    if (Symbol.IsOperation(values[currentIndex]))
-                    {
-                        temp = new NodeOperation(values, ref currentIndex);
-                    }
-                    else
-                    {
-                        temp = new NodeOperand(values, ref currentIndex);
-                    }
-                    if (i == 0)
-                    {
-                        leftSon = temp;
-                    }
-                    else
-                    {
-                        rightSon = temp;
-                    }
-                }
-            }
-
-            public override string Print()
-            {
-                string result = "";
-                result = "( " + operation + " ";
-                result = result + leftSon.Print();
-                result = result + rightSon.Print();
-                result = result + ") ";
-                return result;
-            }
-
-            public override int Calculate()
-            {
-                int firstOperand = leftSon.Calculate();
-                int secondOperand = rightSon.Calculate();
-                int result = 0;
-                switch (operation)
-                {
-                    case "+":
-                        {
-                            result = firstOperand + secondOperand;
-                            break;
-                        }
-                    case "-":
-                        {
-                            result = firstOperand - secondOperand;
-                            break;
-                        }
-                    case "*":
-                        {
-                            result = firstOperand * secondOperand;
-                            break;
-                        }
-                    case "/":
-                        {
-                            if (secondOperand == 0)
-                            {
-                                throw new DividingByZeroException("Dividing by zero.");
-                            }
-                            result = firstOperand / secondOperand;
-                            break;
-                        }
-                }
-                return result;
-            }
         }
 
         /// <summary>
