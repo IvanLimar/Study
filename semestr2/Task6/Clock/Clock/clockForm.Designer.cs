293be0b76e6e45b75662f4785792c54a7f0ac959
Ivan Limar <limvan@mail.ru>
2015-05-12 17:46:00 +0300

    Часы.

---
+++ semestr2/Task6/Clock/Clock/clockForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Clock
+{
+    partial class clockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockPictureBox = new System.Windows.Forms.PictureBox();
+            this.clockTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.clockPictureBox)).BeginInit();
+            this.clockTableLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clockPictureBox
+            // 
+            this.clockPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clockPictureBox.BackColor = System.Drawing.Color.White;
+            this.clockPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.clockPictureBox.Name = "clockPictureBox";
+            this.clockPictureBox.Size = new System.Drawing.Size(282, 265);
+            this.clockPictureBox.TabIndex = 0;
+            this.clockPictureBox.TabStop = false;
+            // 
+            // clockTableLayout
+            // 
+            this.clockTableLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clockTableLayout.ColumnCount = 1;
+            this.clockTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.clockTableLayout.Controls.Add(this.clockPictureBox, 0, 0);
+            this.clockTableLayout.Location = new System.Drawing.Point(0, -5);
+            this.clockTableLayout.Name = "clockTableLayout";
+            this.clockTableLayout.RowCount = 1;
+            this.clockTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.clockTableLayout.Size = new System.Drawing.Size(288, 271);
+            this.clockTableLayout.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.Draw);
+            // 
+            // clockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.clockTableLayout);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "clockForm";
+            this.Text = "Clock";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Draw);
+            ((System.ComponentModel.ISupportInitialize)(this.clockPictureBox)).EndInit();
+            this.clockTableLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox clockPictureBox;
+        private System.Windows.Forms.TableLayoutPanel clockTableLayout;
+        private System.Windows.Forms.Timer timer;
+
+
+
+
+    }
+}
+
---
+++ semestr2/Task6/Clock/Clock/clockForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class clockForm : Form
+    {
+        private Bitmap bitmap;
+        private Graphics graphics;
+
+        private Color color = Color.Black;
+
+        public clockForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Рисуем часы.
+        /// </summary>
+        private void Draw(object sender, EventArgs e)
+        {
+            Init();
+            DrawDial();
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Создаем "холст"
+        /// </summary>
+        private void Init()
+        {
+            bitmap = new Bitmap(clockPictureBox.Width, clockPictureBox.Height);
+            clockPictureBox.Image = bitmap;
+            graphics = Graphics.FromImage(bitmap);
+        }
+
+        /// <summary>
+        /// Ртсуем циферблат.
+        /// </summary>
+        private void DrawDial()
+        {
+            int size = Math.Min(clockPictureBox.Width, clockPictureBox.Height) / 2;
+            DrawCircle(size);
+            DrawMarking(size);
+            DateTime time = DateTime.Now;
+            double hourArgument = (time.Hour % 12) * Math.PI / 6 + time.Minute * Math.PI / 360 - Math.PI / 2;
+            double minuteArgument =  time.Minute * Math.PI / 30 - Math.PI / 2;
+            double secondArgument =  time.Second * Math.PI / 30 - Math.PI / 2;
+            int hourRadius = size - 60;
+            int minuteRadius = size - 30;
+            int secondRadius = size - 20;
+            DrawHand(new Pen(color, 3), hourRadius, hourArgument);
+            DrawHand(new Pen(color, 3), minuteRadius, minuteArgument);
+            DrawHand(new Pen(color, 1), secondRadius, secondArgument);
+        }
+
+        /// <summary>
+        /// Рисуем окружность нужного размера
+        /// </summary>
+        private void DrawCircle(int size)
+        {
+            int x = clockPictureBox.Width / 2 - size + 10;
+            int y = clockPictureBox.Height / 2 - size + 10;
+            int temp = (size - 10) * 2;
+            graphics.DrawEllipse(new Pen(color, 2), new Rectangle(x, y, temp, temp));
+        }
+
+        /// <summary>
+        /// Разметка на циферблате.
+        /// </summary>
+        private void DrawMarking(int size)
+        {
+            int smallRadius, bigRadius;
+            Pen pen;
+            for (int i = 0; i < 60; ++i)
+            {
+                if (i % 5 == 0)
+                {
+                    smallRadius = size - 18;
+                    bigRadius = size - 2;
+                    pen = new Pen(color, 2);
+                }
+                else
+                {
+                    smallRadius = size - 15;
+                    bigRadius = size - 5;
+                    pen = new Pen(color, 1);
+                }
+                double argument = i * Math.PI / 30;
+                float x1 = clockPictureBox.Width / 2 + Convert.ToSingle(smallRadius * Math.Cos(argument));
+                float y1 = clockPictureBox.Height / 2 + Convert.ToSingle(smallRadius * Math.Sin(argument));
+                float x2 = clockPictureBox.Width / 2 + Convert.ToSingle(bigRadius * Math.Cos(argument));
+                float y2 = clockPictureBox.Height / 2 + Convert.ToSingle(bigRadius * Math.Sin(argument));
+                graphics.DrawLine(pen, x1, y1, x2, y2);
+            }
+        }
+
+        /// <summary>
+        /// Стрелка (часовая, минутная, секундная).
+        /// </summary>
+        private void DrawHand(Pen pen, int size, double angle)
+        {
+            float x = clockPictureBox.Width / 2 + Convert.ToSingle(size * Math.Cos(angle));
+            float y = clockPictureBox.Height / 2 + Convert.ToSingle(size * Math.Sin(angle));
+            graphics.DrawLine(pen, clockPictureBox.Width / 2, clockPictureBox.Height / 2, x, y);
+        }
+    }
+}
