e45bdb7e1fb3888e59834aedd6ebcdf74104d1a8
Ivan Limar <limvan@mail.ru>
2015-09-23 15:49:00 +0300

    Delete, Contais + small updates

--- semestr3/BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
+++ semestr3/BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -10,6 +10,33 @@ namespace BinarySearchTree
     public class BinarySearchTree<T> : IEnumerable where T : IComparable
     {
         private Node root;
+
+        /// <summary>
+        /// Gets true if tree is empty.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                return root == null;
+            }
+        }
+
+        /// <summary>
+        /// Gets true if tree is normal
+        /// </summary>
+        public bool IsBinarySearchTree
+        {
+            get
+            {
+                if (root != null)
+                {
+                    return root.IsNormalNode();
+                }
+                return true;
+            }
+        }
+
         /// <summary>
         /// Binary search tree's node.
         /// </summary>
@@ -55,6 +82,139 @@ namespace BinarySearchTree
                 }
             }
 
+            public bool ContainsValue(T value)
+            {
+                int temp = value.CompareTo(Value);
+                if (temp == 0)
+                {
+                    return true;
+                }
+                if (temp > 0)
+                {
+                    if (RightSon == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return RightSon.ContainsValue(value);
+                    }
+                }
+                else
+                {
+                    if (LeftSon == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return LeftSon.ContainsValue(value);
+                    }
+                }
+            }
+
+            public void DeleteValue(ref Node parent, T value)
+            {
+                int temp = value.CompareTo(Value);
+                if (temp == 0)
+                {
+                    bool isRightSon = parent.RightSon == this;
+                    if (RightSon == null)
+                    {
+                        if (LeftSon == null)
+                        {
+                            if (isRightSon)
+                            {
+                                parent.RightSon = null;
+                            }
+                            else
+                            {
+                                parent.LeftSon = null;
+                            }
+                        }
+                        else
+                        {
+                            if (isRightSon)
+                            {
+                                parent.RightSon = LeftSon;
+                            }
+                            else
+                            {
+                                parent.LeftSon = LeftSon;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (LeftSon == null)
+                        {
+                            if (isRightSon)
+                            {
+                                parent.RightSon = RightSon;
+                            }
+                            else
+                            {
+                                parent.LeftSon = RightSon;
+                            }
+                        }
+                        else
+                        {
+                            Node leftNode = RightSon.leftNode();
+                            T swaper = Value;
+                            Value = leftNode.Value;
+                            leftNode.Value = swaper;
+                            parent = this;
+                            LeftSon.DeleteValue(ref parent, value);
+                        }
+                    }
+                    return;
+                }
+
+                parent = this;
+                if (temp > 0)
+                {
+                    if (RightSon != null)
+                    {
+                        RightSon.DeleteValue(ref parent, value);
+                    }
+                }
+                else
+                {
+                    if (LeftSon != null)
+                    {
+                        LeftSon.DeleteValue(ref parent, value);
+                    }
+                }
+            }
+
+            private Node leftNode()
+            {
+                if (LeftSon != null)
+                {
+                    return LeftSon.leftNode();
+                }
+                return this;
+            }
+
+            public bool IsNormalNode()
+            {
+                if (RightSon == null)
+                {
+                    if (LeftSon == null)
+                    {
+                        return true;
+                    }
+                    return Value.CompareTo(LeftSon.Value) > 0 && LeftSon.IsNormalNode();
+                }
+                else
+                {
+                    if (LeftSon == null)
+                    {
+                        return Value.CompareTo(RightSon.Value) < 0 && RightSon.IsNormalNode();
+                    }
+                    return Value.CompareTo(LeftSon.Value) > 0 && Value.CompareTo(RightSon.Value) < 0 && LeftSon.IsNormalNode() && RightSon.IsNormalNode();
+                }
+            }
         }
 
         /// <summary>
@@ -68,6 +228,33 @@ namespace BinarySearchTree
                 return;
             }
             root.AddValue(value);
+        }
+
+        /// <summary>
+        /// Returns true if value is in tree.
+        /// </summary>
+        public bool Contains(T value)
+        {
+            if (root != null)
+            {
+                return root.ContainsValue(value);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Deletes value from tree.
+        /// </summary>
+        public void Delete(T value)
+        {
+            if (root != null)
+            {
+                Node temp = new Node(default(T));
+                temp.RightSon = root;
+                Node parent = temp;
+                root.DeleteValue(ref parent, value);
+                root = temp.RightSon;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
--- semestr3/BinarySearchTree/BinarySearchTreeTest/BinarySearchTreeTest.cs
+++ semestr3/BinarySearchTree/BinarySearchTreeTest/BinarySearchTreeTest.cs
@@ -8,31 +8,42 @@ namespace BinarySearchTreeTest
     [TestClass]
     public class BinarySearchTreeTest
     {
-        private BinarySearchTree<int> tree;
+        private BinarySearchTree<int> usualTree;
+        private BinarySearchTree<int> listTree;
+        private BinarySearchTree<int> emptyTree;
         private List<int> values;
 
         [TestInitialize]
         public void TestInit()
         {
-            tree = new BinarySearchTree<int>();
+            usualTree = new BinarySearchTree<int>();
+            listTree = new BinarySearchTree<int>();
+            emptyTree = new BinarySearchTree<int>();
             values = new List<int>();
+
+            usualTree.Add(5);
+            usualTree.Add(7);
+            usualTree.Add(3);
+            usualTree.Add(9);
+            usualTree.Add(2);
+            usualTree.Add(4);
+            usualTree.Add(6);
+            usualTree.Add(1);
+            usualTree.Add(10);
+            usualTree.Add(15);
+            usualTree.Add(13);
+            usualTree.Add(14);
+
+            for (int i = 0; i < 10; ++i)
+            {
+                listTree.Add(i);
+            }
         }
 
         [TestMethod]
         public void TestOne()
         {
-            tree.Add(5);
-            tree.Add(7);
-            tree.Add(3);
-            tree.Add(9);
-            tree.Add(2);
-            tree.Add(4);
-            tree.Add(6);
-            tree.Add(1);
-            tree.Add(10);
-            tree.Add(15);
-            tree.Add(13);
-            tree.Add(14);
+            
             values.Add(5);
             values.Add(3);
             values.Add(2);
@@ -46,7 +57,7 @@ namespace BinarySearchTreeTest
             values.Add(13);
             values.Add(14);
             int i = 0;
-            foreach (var x in tree)
+            foreach (var x in usualTree)
             {
                 Assert.AreEqual(x, values[i++]);
             }
@@ -57,11 +68,10 @@ namespace BinarySearchTreeTest
         {
             for (int i = 0; i < 10; ++i)
             {
-                tree.Add(i);
                 values.Add(i);
             }
             int y = 0;
-            foreach (var x in tree)
+            foreach (var x in listTree)
             {
                 Assert.AreEqual(x, values[y++]);
             }
@@ -70,9 +80,46 @@ namespace BinarySearchTreeTest
         [TestMethod]
         public void EmptyTreeTest()
         {
-            foreach (var x in tree)
+            foreach (var x in emptyTree)
             {
             }
+            Assert.IsTrue(emptyTree.IsEmpty);
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                Assert.IsTrue(listTree.Contains(i));
+            }
+            Assert.IsFalse(emptyTree.Contains(10));
+            Assert.IsTrue(usualTree.Contains(7));
+            Assert.IsFalse(usualTree.Contains(100));
+        }
+
+        [TestMethod]
+        public void SimplestDeleteTest()
+        {
+            usualTree.Delete(15);
+            Assert.IsTrue(usualTree.IsBinarySearchTree);
+            Assert.IsFalse(usualTree.Contains(15));
+        }
+
+        [TestMethod]
+        public void SimpleDeleteTest()
+        {
+            listTree.Delete(7);
+            Assert.IsTrue(listTree.IsBinarySearchTree);
+            Assert.IsFalse(listTree.Contains(7));
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            usualTree.Delete(5);
+            Assert.IsTrue(usualTree.IsBinarySearchTree);
+            Assert.IsFalse(usualTree.Contains(5));
         }
     }
 }
