ec479f649712271d631e95f59f6a08dbb8d407bb
Ivan Limar <limvan@mail.ru>
2015-10-28 21:31:42 +0300

    Simple editor

---
+++ semestr3/SimpleEditor/SimpleEditor/Core.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleEditor
+{
+    public class Core
+    {
+        private List<Line> lines;
+        private Line activeLine;
+        private State state;
+        private bool isLineRemoving;
+        private int numberOfRemovingEnd;
+
+        public bool IsLineRemoving
+        {
+            get
+            {
+                return isLineRemoving;
+            }
+        }
+
+        public Line ActiveLine
+        {
+            get
+            {
+                return activeLine;
+            }
+        }
+
+        public List<Line> Lines
+        {
+            get
+            {
+                return lines;
+            }
+        }
+
+        public Core()
+        {
+            lines = new List<Line>();
+            state = new State();
+            activeLine = null;
+            isLineRemoving = false;
+            numberOfRemovingEnd = 0;
+        }
+
+        /// <summary>
+        /// Sets active line, which user can delete or remove end of it.
+        /// </summary>
+        public void SetActiveLine(float xCoordinate, float yCoordinate)
+        {
+            foreach (var x in lines)
+            {
+                if (x.IsPointNear(xCoordinate, yCoordinate))
+                {
+                    activeLine = x;
+                    return;
+                }
+            }
+            activeLine = null;
+            numberOfRemovingEnd = 0;
+            isLineRemoving = false;
+        }
+
+        /// <summary>
+        /// Creates new line.
+        /// </summary>
+        public void AddLine()
+        {
+            const float firstPoint = 10;
+            const float secondPoint = 20;
+            Line line = new Line(firstPoint, firstPoint, secondPoint, secondPoint);
+            lines.Add(line);
+        }
+
+        /// <summary>
+        /// Deletes active line
+        /// </summary>
+        public void DeleteLine()
+        {
+            if (activeLine != null)
+            {
+                lines.Remove(activeLine);
+                activeLine = null;
+                isLineRemoving = false;
+                numberOfRemovingEnd = 0;
+            }
+        }
+
+        /// <summary>
+        /// Sets line, which end will be removed.
+        /// </summary>
+        public void SetRemovingLine(float xCoordinate, float yCoordinate)
+        {
+            numberOfRemovingEnd = activeLine.NumberOfRemovingEnd(xCoordinate, yCoordinate);
+            if (numberOfRemovingEnd != 0)
+            {
+                isLineRemoving = true;
+            }
+        }
+
+        /// <summary>
+        /// Changes the coordinates of line's end.
+        /// </summary>
+        public void ChangeEnd(float xCoordinate, float yCoordinate)
+        {
+            activeLine.ChangeEnd(numberOfRemovingEnd, xCoordinate, yCoordinate);
+        }
+
+        public void ChangingComplete()
+        {
+            numberOfRemovingEnd = 0;
+            isLineRemoving = false;
+        }
+
+        public void UpdadeState()
+        {
+            List<Line> temp = CloneLines();
+            state.UpdateState(temp);
+        }
+
+        public void Undo()
+        {
+            List<Line> argument = CloneLines();
+            List<Line> temp = state.PreviousState(argument);
+            if (temp != null)
+            {
+                lines = temp;
+            }
+        }
+
+        public void Redo()
+        {
+            List<Line> argument = CloneLines();
+            List<Line> temp = state.CanceledState(new List<Line>(argument));
+            if (temp != null)
+            {
+                lines = temp;
+            }
+        }
+
+        private List<Line> CloneLines()
+        {
+            List<Line> result = new List<Line>();
+            for (int i = 0; i < lines.Count; ++i)
+            {
+                result.Add(new Line(lines[i].Coordinates[0], lines[i].Coordinates[1], lines[i].Coordinates[2], lines[i].Coordinates[3]));
+            }
+            return result;
+        }
+    }
+}
---
+++ semestr3/SimpleEditor/SimpleEditor/SimpleEditorForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimpleEditor
+{
+    public partial class SimpleEditorForm : Form
+    {
+        private Core core;
+        private Bitmap bitmap;
+        private Graphics graphics;
+
+        public SimpleEditorForm()
+        {
+            InitializeComponent();
+            pictureBox.BackColor = Color.White;
+            core = new Core();
+        }
+
+        private void Init()
+        {
+            bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = bitmap;
+            graphics = Graphics.FromImage(bitmap);
+        }
+
+        private void DrawAllLines()
+        {
+            Init();
+            Pen pen = new Pen(Color.Black, 2);
+            foreach (var x in core.Lines)
+            {
+                float x1 = x.Coordinates[0] * pictureBox.Width / 100;
+                float y1 = x.Coordinates[1] * pictureBox.Height / 100;
+                float x2 = x.Coordinates[2] * pictureBox.Width / 100;
+                float y2 = x.Coordinates[3] * pictureBox.Height / 100;
+                graphics.DrawLine(pen, x1, y1, x2, y2);
+            }
+        }
+
+        private void addLineButton_Click(object sender, EventArgs e)
+        {
+            core.UpdadeState();
+            core.AddLine();
+            DrawAllLines();
+        }
+
+        private void SimpleEditorForm_Resize(object sender, EventArgs e)
+        {
+            DrawAllLines();
+        }
+
+        private void deleteLineButton_Click(object sender, EventArgs e)
+        {
+            if (core.ActiveLine != null)
+            {
+                core.UpdadeState();
+                core.DeleteLine();
+                DrawAllLines();
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            float xCoordinate = e.X * 100 / pictureBox.Width;
+            float yCoordinate = e.Y * 100 / pictureBox.Height;
+            core.SetActiveLine(xCoordinate, yCoordinate);
+            DrawAllLines();
+            if (core.ActiveLine != null)
+            {
+                core.SetRemovingLine(xCoordinate, yCoordinate);
+                if (core.IsLineRemoving)
+                {
+                    core.UpdadeState();
+                }
+                Line activeLine = core.ActiveLine;
+                DrawActiveLine(activeLine);
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (core.IsLineRemoving)
+            {
+                ChangeEnd(e);
+                core.ChangingComplete();
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (core.IsLineRemoving)
+            {
+                ChangeEnd(e);
+            }
+        }
+
+        private void ChangeEnd(MouseEventArgs e)
+        {
+            float xCoordinate = e.X * 100 / pictureBox.Width;
+            float yCoordinate = e.Y * 100 / pictureBox.Height;
+            core.ChangeEnd(xCoordinate, yCoordinate);
+            Line activeLine = core.ActiveLine;
+            DrawAllLines();
+            DrawActiveLine(activeLine);
+        }
+
+        private void DrawActiveLine(Line activeLine)
+        {
+            float x1 = activeLine.Coordinates[0] * pictureBox.Width / 100;
+            float y1 = activeLine.Coordinates[1] * pictureBox.Height / 100;
+            float x2 = activeLine.Coordinates[2] * pictureBox.Width / 100;
+            float y2 = activeLine.Coordinates[3] * pictureBox.Height / 100;
+            graphics.DrawLine(new Pen(Color.Red, 2), x1, y1, x2, y2);
+        }
+
+        private void undoButton_Click(object sender, EventArgs e)
+        {
+            core.Undo();
+            DrawAllLines();
+        }
+
+        private void redoButton_Click(object sender, EventArgs e)
+        {
+            core.Redo();
+            DrawAllLines();
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            Init();
+            string help = "To create new line click \r\n" +
+                          "'Add line'. To delete line \r\n" +
+                          "       click on line (it is \r\n" +
+                          "   repainted into red) and \r\n" +
+                          "   press 'Delete line'. \r\n" +
+                          "  Click here to continue. \r\n";
+            graphics.DrawString(help, new Font(Font.FontFamily, pictureBox.Height / 20), new SolidBrush(Color.Black), pictureBox.Width / 10 , 10);
+        }
+    }
+}
