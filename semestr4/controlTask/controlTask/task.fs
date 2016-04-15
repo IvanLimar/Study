93e37d0b0678e2559a5113831acc5075d2221d1a
Ivan Limar <limvan@mail.ru>
2016-04-15 12:18:01 +0300

    supermap and the biggest palindrome

--- semestr4/controlTask/controlTask/task.fs
+++ semestr4/controlTask/controlTask/task.fs
@@ -28,3 +28,38 @@ let y = stack.Pop
 let z = stack.Pop
 let c = stack.IsEmpty
 let k = stack.Pop
+
+let theBiggestPalindrome =
+    let rec findMax ls acc =
+        let rec findMaxLoop ls accumulator =
+            match ls with
+                | (head :: tail) -> let isPalindrome number =
+                                        let ls = [for i in number.ToString() -> i]
+                                        (=) ls (List.rev ls)
+                                    if isPalindrome head
+                                    then if head > accumulator then findMaxLoop tail head
+                                                               else findMaxLoop tail accumulator
+                                    else findMaxLoop tail accumulator
+                | [] -> accumulator
+        match ls with
+            | (head :: tail) ->  let temp = findMaxLoop (List.map ((*) head) [100..999]) acc
+                                 if temp > acc then findMax tail temp
+                                               else findMax tail acc                                
+            | [] -> acc
+    findMax [100..999] 0
+    
+let result = theBiggestPalindrome
+
+let superMap func ls =
+    let rec loop ls acc =
+        let rec innerLoop ls acc =
+            match ls with
+                | (head :: tail) -> innerLoop tail (head :: acc)
+                | [] -> acc
+        match ls with
+            | (head :: tail) -> let accumulator = innerLoop (List.rev (func head)) acc
+                                loop tail accumulator
+            | [] -> acc
+    loop (List.rev ls) List.empty
+    
+let resultSuperMapResult = superMap (fun x -> [x; x * x]) [2.0; 3.0]
