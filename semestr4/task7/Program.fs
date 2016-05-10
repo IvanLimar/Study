b180cea88bf9460ab3e19a5bdd977c86cdcc33d5
Ivan Limar <limvan@mail.ru>
2016-05-10 20:30:28 +0300

    Fixes

--- semestr4/task7/Program.fs
+++ semestr4/task7/Program.fs
@@ -10,23 +10,26 @@ let fetch (url : string) =
     use reader = new StreamReader(stream)
     let text = reader.ReadToEnd()
     let regularExpression = new Regex("<a href=\"http://.+?\">")
-    let urls = [for i in regularExpression.Matches(text) ->
+    let urls = seq {for i in regularExpression.Matches(text) ->
                     let regularExpression = new Regex("\"http://.+?\"")
                     let first = regularExpression.Match(i.ToString())
                     let second = first.ToString()
-                    second.Substring(1, second.Length - 2)]
+                    second.Substring(1, second.Length - 2) }
     let fetch (url : string) =
         async {
-            let request = WebRequest.Create(url)
-            use! response = request.AsyncGetResponse()
-            use stream = response.GetResponseStream()
-            use reader = new StreamReader(stream)
-            let text = reader.ReadToEnd()
-            let tprintfn ftm =
-                printf " Thread %d " Thread.CurrentThread.ManagedThreadId
-                printfn ftm
-            do tprintfn "%s - %d" url text.Length
+            try
+                let request = WebRequest.Create(url)
+                use! response = request.AsyncGetResponse()
+                use stream = response.GetResponseStream()
+                use reader = new StreamReader(stream)
+                let text = reader.ReadToEnd()
+                let tprintfn ftm =
+                    printf " Thread %d " Thread.CurrentThread.ManagedThreadId
+                    printfn ftm
+                do tprintfn "%s - %d" url text.Length
+            with 
+                | ex -> printfn "%s" ex.Message
         }
-    ignore (List.map (fun x -> Async.Start (fetch x)) urls)
+    urls |> Seq.map (fun x -> fetch x) |> Async.Parallel |> Async.RunSynchronously |> ignore
     
-fetch "http://math.spbu.ru"
+fetch "http://football.kulichki.net"
