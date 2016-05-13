8925e25fdda6ba17511dc4db01551be7087aaf4a
Ivan Limar <limvan@mail.ru>
2016-05-13 07:28:53 +0300

    Small fixes

--- semestr4/task8/Task8.fs
+++ semestr4/task8/Task8.fs
@@ -3,12 +3,12 @@ open System.Threading
 
 let sum =
 
-    let array = [|for i in 1..1000001 -> 1|]
+    let array = [|for i in 1..1000000 -> 1|]
     
     let result = ref 0
 
     let threads = [for i in 1..100 -> new Thread(ThreadStart(fun _ -> Thread.CurrentThread.Name <- i.ToString()
-                                                                      let startIndex =  1 + 10000 * (System.Int32.Parse(Thread.CurrentThread.Name) - 1)
+                                                                      let startIndex =  10000 * (System.Int32.Parse(Thread.CurrentThread.Name) - 1)
                                                                       for j in startIndex..startIndex + 9999 do
                                                                           Monitor.Enter(result)
                                                                           try
