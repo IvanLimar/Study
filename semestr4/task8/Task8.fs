2f7314afe899a3039385fee50dc86059114e65d1
Ivan Limar <limvan@mail.ru>
2016-05-20 19:05:18 +0300

    Added flags

--- semestr4/task8/Task8.fs
+++ semestr4/task8/Task8.fs
@@ -5,6 +5,8 @@ let sum =
 
     let array = [|for i in 1..1000000 -> 1|]
     
+    let flags = [|for i in 1..100 -> false|]
+    
     let result = ref 0
 
     let threads = [for i in 0..99 -> 
@@ -15,10 +17,13 @@ let sum =
                                    | [] -> acc
                            let startIndex = 10000 * i
                            let partialSum = loop [startIndex..startIndex + 9999] 0
+                           flags.[i] <- true
                            result := !result + partialSum))]
 
     for i in threads do 
         i.Start()
+
+    while (not (Array.forall id flags)) do ()
  
     !result
 
